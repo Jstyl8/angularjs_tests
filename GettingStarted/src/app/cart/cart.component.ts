1a03503e85050acab72464d58dad2190886ee144
Jstyl8 <joseangel.jstyl8@gmail.com>
2019-09-08 14:06:42 +0200

    GettingStarted Step6. Add submit form

--- GettingStarted/src/app/cart/cart.component.ts
+++ GettingStarted/src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from "@angular/forms";
 import {CartService} from "../cart.service";
+
 
 @Component({
   selector: 'app-cart',
@@ -8,14 +10,33 @@ import {CartService} from "../cart.service";
 })
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) {
+    this.fillItems();
+
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
   ngOnInit() {
-    this.items = this.cartService.getItems();
+    this.fillItems();
   }
 
+  private fillItems() {
+    this.items = this.cartService.getItems();
+  }
 }
