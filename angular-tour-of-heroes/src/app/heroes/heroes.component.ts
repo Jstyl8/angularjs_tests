809bfb85fd7e26f080f551dbd20d80c90ec8405a
Jstyl8 <joseangel.jstyl8@gmail.com>
2019-09-08 16:11:55 +0200

    TourOfHeroes-Selected hero support

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,14 +9,14 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  selectedHero: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
